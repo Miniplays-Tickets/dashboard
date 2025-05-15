8fd20f030fbe37d9dbfc45966da242d27b8255b1
Stefan279 <stefan@miniplays.de>
2025-05-15 18:17:31 +0200

    Update Navbar.svelte
    Signed-off-by: Stefan279 <stefan@miniplays.de>

--- frontend/src/includes/Navbar.svelte
+++ frontend/src/includes/Navbar.svelte
@@ -17,8 +17,8 @@
 
         {#if isAdmin}
           <NavElement icon="fas fa-mouse-pointer" link="/manage/{guildId}/panels" on:click={closeDropdown}>Ticket Panels</NavElement>
-          <NavElement icon="fas fa-poll-h" link="/manage/{guildId}/forms" on:click={closeDropdown}>Forms</NavElement>
-          <NavElement icon="fas fa-users" link="/manage/{guildId}/teams" on:click={closeDropdown}>Staff Teams</NavElement>
+          <NavElement icon="fas fa-poll-h" link="/manage/{guildId}/forms" on:click={closeDropdown}>Formulare</NavElement>
+          <NavElement icon="fas fa-users" link="/manage/{guildId}/teams" on:click={closeDropdown}>Teams</NavElement>
           <NavElement icon="fas fa-robot" link="/manage/{guildId}/integrations" on:click={closeDropdown}>Integrations</NavElement>
           <NavElement icon="fas fa-file-import" link="/manage/{guildId}/import" on:click={closeDropdown}>Import</NavElement>
         {/if}
